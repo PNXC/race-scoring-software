78a44a7e98f03b182e1904a58dc5bc3907948a92
Daniel Gaull <danieljgaull@gmail.com>
2025-10-10 22:29:35 -0700

    Added test for edit person

--- tests/IntegrationTests/PersonFunctionTests.cs
+++ tests/IntegrationTests/PersonFunctionTests.cs
@@ -12,7 +12,7 @@ namespace tests.IntegrationTests;
 
 public class PersonFunctionTests : IClassFixture<ServerFixture>
 {
-     private readonly HttpClient _client;
+    private readonly HttpClient _client;
 
     public PersonFunctionTests(ServerFixture fixture)
     {
@@ -52,6 +52,50 @@ public class PersonFunctionTests : IClassFixture<ServerFixture>
             Assert.Equal(createDto.LastName, createdPerson.LastName);
             Assert.Equal(createDto.Email, createdPerson.Email);
             Assert.Equal(createDto.PhoneNumber, createdPerson.PhoneNumber);
+        }
+        finally
+        {
+            if (personId is not null)
+            {
+                await _client.DeleteAsync($"/persons/{personId}");
+            }
+        }
+    }
+    
+    [Fact]
+    public async Task EditPerson_Works()
+    {
+        var createDto = new CreatePersonDto
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test-user@email.com",
+            PhoneNumber = "1-234-567-8900",
+        };
+
+        int? personId = null;
+        try
+        {
+            var createResponse = await _client.PostAsJsonAsync("api/persons", createDto);
+            createResponse.EnsureSuccessStatusCode();
+            var createdPerson = await createResponse.Content.ReadFromJsonAsync<PersonDto>();
+            personId = createdPerson.PersonId;
+
+            var editDto = new UpdatePersonDto
+            {
+                FirstName = "Test upated",
+                LastName = "User updated",
+                Email = "test-user2@email.com",
+                PhoneNumber = "2-234-567-8900",
+            };
+            var editResponse = await _client.PutAsJsonAsync($"api/persons/{personId}", editDto);
+            var editedPerson = await editResponse.Content.ReadFromJsonAsync<PersonDto>();
+
+            Assert.Equal(HttpStatusCode.OK, editResponse.StatusCode);
+            Assert.Equal(editDto.FirstName, editedPerson.FirstName);
+            Assert.Equal(editDto.LastName, editedPerson.LastName);
+            Assert.Equal(editDto.Email, editedPerson.Email);
+            Assert.Equal(editDto.PhoneNumber, editedPerson.PhoneNumber);
         }
         finally
         {
