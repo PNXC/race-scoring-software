082b8a3ad62bc6f1e3db36f6c41faf24a4a5d84e
Daniel Gaull <danieljgaull@gmail.com>
2025-10-11 13:06:02 -0700

    Sortable Table component

---
+++ ui/src/components/SortableTable/SortableTable.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { IoChevronUpCircle, IoChevronDownCircle  } from "react-icons/io5";
+import cx from 'classnames';
+
+import './SortableTable.css';
+
+// TODO: these types are a bit messed up. Sortable should only be possible if dataSource is provided
+// TODO: this component only works in cases where all data is already loaded into memory
+/// What if we want a paged endpoint?
+// TODO: custom display fn (ex. for time, we want to sort on total seconds but display as mm:ss)
+
+type ColumnData<V> = { dataSource: keyof V, content?: never } | { content: (v: V) => React.ReactNode, dataSource?: never };
+type Column<V> = {
+    title: string;
+    sortable?: boolean;
+} & ColumnData<V>;
+interface SortableTableProps<V extends Record<string, any>> {
+    columns: Column<V>[];
+    data: V[];
+    defaultSortedColumn: keyof V;
+}
+function SortableTable<V extends Record<string, any>>({ columns, data, defaultSortedColumn }: SortableTableProps<V>) {
+    const [sortedColumn, setSortedColumn] = useState<keyof V>(defaultSortedColumn);
+    const [sortAsc, setSortAsc] = useState<boolean>(false);
+    const [sortedData, setSortedData] = useState<V[]>(data);
+
+    const onSortClick = useCallback((column: keyof V) => {
+        if (sortedColumn === column) {
+            setSortAsc(v => !v);
+        } else {
+            setSortAsc(false);
+            setSortedColumn(column);
+        }
+    }, [sortedColumn]);
+
+    const SortChevron = useMemo(() => ({ column }: { column: keyof V }) => {
+        return (
+            <span className="chevron">
+                &nbsp;
+                {sortAsc ? <IoChevronUpCircle /> : <IoChevronDownCircle />}
+            </span>
+        );
+    }, [sortAsc]);
+
+    useEffect(() => {
+        const sorted = [...data].sort((a, b) => {
+            const va = a[sortedColumn];
+            const vb = b[sortedColumn];
+
+            if (typeof va === 'number' && typeof vb === 'number') {
+                return va - vb;
+            }
+
+            const stra = String(va).toLowerCase();
+            const strb = String(vb).toLowerCase();
+            if (!sortAsc) {
+                if (stra < strb) return -1;
+                if (stra > strb) return 1;
+            } else {
+                if (stra < strb) return 1;
+                if (stra > strb) return -1;
+            }
+            return 0;
+        });
+        setSortedData(sorted);
+    }, [sortAsc, sortedColumn, data]);
+
+    return (
+        <table>
+            <tr>
+                {columns.map((c, idx) => (
+                    <th
+                        key={idx}
+                        onClick={() => {
+                            if (c.dataSource && c.sortable) {
+                                onSortClick(c.dataSource);
+                            }
+                        }}
+                        className={cx({ sortable: c.dataSource && c.sortable })}
+                    >
+                        {c.title}
+                        {c.dataSource === sortedColumn && c.sortable && (
+                            <SortChevron column={c.dataSource} />
+                        )}
+                    </th>
+                ))}
+            </tr>
+            {sortedData.map((row, idx) => (
+                <tr key={idx}>
+                    {columns.map((col, idx) => (
+                        <td key={idx}>
+                            {col.dataSource ? row[col.dataSource] : col.content?.(row)}
+                        </td>
+                    ))}
+                </tr>
+            ))}
+        </table>
+    );
+}
+
+export default SortableTable;
--- ui/src/pages/Person/PersonList.tsx
+++ ui/src/pages/Person/PersonList.tsx
@@ -5,6 +5,7 @@ import api from "../../api/api";
 import Button from "../../components/Button/Button";
 import { FaPencil } from "react-icons/fa6";
 import { FaTrashAlt } from "react-icons/fa";
+import SortableTable from "../../components/SortableTable/SortableTable";
 
 const PersonList = () => {
     const [people, setPeople] = useState<PersonDto[]>([]);
@@ -22,43 +23,51 @@ const PersonList = () => {
         <Page title="People">
             <Button linkTo="/new-person">+ Create Person</Button>
 
-            <table>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th></th>
-                </tr>
-                {people.map(person => (
-                    <React.Fragment key={person.PersonId}>
-                        <tr>
-                            <td>{person.FirstName}</td>
-                            <td>{person.LastName}</td>
-                            <td>{person.Email}</td>
-                            <td>{person.PhoneNumber}</td>
-                            <td>
-                                <div className="center">
-                                    <Button type="primary" small linkTo={`/edit-person/${person.PersonId}`}>
-                                        <FaPencil />
-                                    </Button>
-                                    {/* TODO: delete confirmation modal, or page */}
-                                    <Button
-                                        type="deny"
-                                        small
-                                        onClick={async () => {
-                                            await api.deletePerson(person.PersonId);
-                                            await loadPeople();
-                                        }}
-                                    >
-                                        <FaTrashAlt />
-                                    </Button>
-                                </div>
-                            </td>
-                        </tr>
-                    </React.Fragment>
-                ))}
-            </table>
+            {/* TODO: delete confirmation modal, or page */}
+            <SortableTable
+                defaultSortedColumn="LastName"
+                data={people}
+                columns={[
+                    {
+                        title: 'First Name',
+                        dataSource: 'FirstName',
+                        sortable: true,
+                    },
+                    {
+                        title: 'Last Name',
+                        dataSource: 'LastName',
+                        sortable: true,
+                    },
+                    {
+                        title: 'Email',
+                        dataSource: 'Email',
+                    },
+                    {
+                        title: 'Phone Number',
+                        dataSource: 'PhoneNumber',
+                    },
+                    {
+                        title: '',
+                        content: person => (
+                            <div className="center">
+                                <Button type="primary" small linkTo={`/edit-person/${person.PersonId}`}>
+                                    <FaPencil />
+                                </Button>
+                                <Button
+                                    type="deny"
+                                    small
+                                    onClick={async () => {
+                                        await api.deletePerson(person.PersonId);
+                                        await loadPeople();
+                                    }}
+                                >
+                                    <FaTrashAlt />
+                                </Button>
+                            </div>
+                        )
+                    }
+                ]}
+            />
         </Page>
     );
 };
